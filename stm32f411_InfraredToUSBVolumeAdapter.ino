517725d58480b81023be188a48d5fb3adf788ee5
SuperThunder <alexander.wolfe.10@googlemail.com>
2021-05-27 03:17:38 -0400

    clean up code a bit

--- stm32f411_InfraredToUSBVolumeAdapter.ino
+++ stm32f411_InfraredToUSBVolumeAdapter.ino
@@ -1,34 +1,5 @@
 /*
- * ReceiveDemo.cpp
- *
- * Demonstrates receiving IR codes with the IRremote library.
- * If debug button is pressed (pin connected to ground) a long output is generated.
- *
- *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
- *
- ************************************************************************************
  * MIT License
- *
- * Copyright (c) 2020-2021 Armin Joachimsmeyer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- ************************************************************************************
  */
 
 //Using shiriff irremote and onetransistor smashed together examples :)
@@ -76,10 +47,7 @@ void setup() {
 #endif
 
     pinMode(DEBUG_BUTTON_PIN, INPUT_PULLUP);
-
-
-// Just to know which program is running on my Arduino
-    Serial1.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    
 
 // In case the interrupt driver crashes on setup, give a clue
 // to the user what's going on.
@@ -88,7 +56,7 @@ void setup() {
     /*
      * Start the receiver, enable feedback LED and (if not 3. parameter specified) take LED feedback pin from the internal boards definition
      */
-    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    IrReceiver.begin(IR_RECEIVE_PIN, DISABLE_LED_FEEDBACK);
 
     Serial1.print(F("Ready to receive IR signals at pin "));
 
@@ -189,8 +157,5 @@ void loop() {
                 break;
             }
         }
-
-        
     }
-
 }
